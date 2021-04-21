6869c480b06d8e92b1eb6601a1411450b5036573
Valdismin <Valdismin@gmail.com>
2021-04-21 19:05:06 +0300

    Updates

---
+++ precache-manifest.7141b20c0d71098ff28d98518eb1ba81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13af5fc4815528dde65e176e1d708180",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "cbf7bc21fd74b3471154",
+    "url": "/todolist/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "2f5ecfc34da8e6a40eb6",
+    "url": "/todolist/static/js/2.7df28b86.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/todolist/static/js/2.7df28b86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbf7bc21fd74b3471154",
+    "url": "/todolist/static/js/main.6a950ef9.chunk.js"
+  },
+  {
+    "revision": "a389fdae866cc75e956d",
+    "url": "/todolist/static/js/runtime-main.f4f4a006.js"
+  }
+]);
